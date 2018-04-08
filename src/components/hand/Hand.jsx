19a0d2ec84fa9a88487e1e79571cdcdea2f954c3
Lucas Alan Silvestri <silvestrilucas@outlook.com>
2018-04-08 17:01:25 -0300

    refactor hand

--- src/components/hand/Hand.jsx
+++ src/components/hand/Hand.jsx
@@ -13,57 +13,16 @@ export class Hand extends Component {
         }
     }
 
-    firstCard = () => (
-        this.state.owner ?
-            <figure className="first">{this.state.cards[0]}</figure> :
-            <figure className="enemy-first"><CardSleeve></CardSleeve></figure>
-    )
-
-    secondCard = () => (
-        this.state.owner ?
-            <figure className="second">{this.state.cards[1]}</figure> :
-            <figure className="enemy-second"><CardSleeve></CardSleeve></figure>
-    )
-
-    thirdCard = () => (
-        this.state.owner ?
-            <figure className="third">{this.state.cards[2]}</figure> :
-            <figure className="enemy-third"><CardSleeve></CardSleeve></figure>
-    )
-
-    fourthCard = () => (
-        this.state.owner ?
-            <figure className="fourth">{this.state.cards[3]}</figure> :
-            <figure className="enemy-fourth"><CardSleeve></CardSleeve></figure>
-    )
-
-    fifthCard = () => (
-        this.state.owner ?
-            <figure className="fifth">{this.state.cards[4]}</figure> :
-            <figure className="enemy-fifth"><CardSleeve></CardSleeve></figure>
-    )
-
-    hand = () => (
-        <section className="hand">
-            {this.firstCard()}
-            {this.secondCard()}
-            {this.thirdCard()}
-            {this.fourthCard()}
-            {this.fifthCard()}
-        </section>
-    )
-
     render = () => (
         <div>
             <div className="hand-container">
-                {/* <section className="hand">
-                    {this.firstCard()}
-                    {this.secondCard()}
-                    {this.thirdCard()}
-                    {this.fourthCard()}
-                    {this.fifthCard()}
-                </section> */}
-                {this.hand()}
+                <section className="hand">
+                    <figure className={`${this.state.owner ? 'first' : 'enemy-first'}`}>{this.state.cards[0]}</figure>
+                    <figure className={`${this.state.owner ? 'second' : 'enemy-second'}`}>{this.state.cards[1]}</figure>
+                    <figure className={`${this.state.owner ? 'third' : 'enemy-third'}`}>{this.state.cards[2]}</figure>
+                    <figure className={`${this.state.owner ? 'fourth' : 'enemy-fourth'}`}>{this.state.cards[3]}</figure>
+                    <figure className={`${this.state.owner ? 'fifth' : 'enemy-fifth'}`}>{this.state.cards[4]}</figure>
+                </section>
             </div>
         </div>
     )
