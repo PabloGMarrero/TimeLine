d731e7325ebb171f6f74703c3eadd7e2f79953a2
unknown <p@gmail.com>
2018-04-14 16:36:51 -0300

    Added translateX in 3D deck

--- src/components/deck/Deck.jsx
+++ src/components/deck/Deck.jsx
@@ -15,10 +15,9 @@ export class Deck extends Component {
             <div className="deck-container">
                 <section className="deck">
                     {this.props.cards.map(card =>
-                        //<div style={{ transform: `translateX(${(this.state.xOffset--)}px)` }}>
-                          //  <Card {...card} flipped={true} visible={false}></Card>
-                        //</div>)}
-                        <Card {...card} flipped={true} visible={false}></Card>)}
+                        <div style={{ transform: `translateX(${(this.state.xOffset--)}px)` }}>
+                            <Card {...card} flipped={true} visible={false}></Card>
+                        </div>)}
                 </section>
             </div>
         </div>
