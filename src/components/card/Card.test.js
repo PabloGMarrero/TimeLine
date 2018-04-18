8b85b96d49bd01560cec4ce6abcc6bcf3d29ebca
PabloGMarrero <pablo.g.marrero@gmail.com>
2018-04-18 19:34:31 -0300

    Testing component card (#1)
    * Added more basic tests in creation of component card
    
    * Tested cardsleeve component creation
    
    * Tested card receive 5 props

--- src/components/card/Card.test.js
+++ src/components/card/Card.test.js
@@ -3,10 +3,9 @@ import { shallow } from 'enzyme'
 import { mount } from 'enzyme'
 
 import {Card} from "./Card.jsx"
+import { not } from "glamor";
 
 it("renderiza campos base de carta", ()=> {
-
-
     const beethoven =
     {
         category: 'p',
@@ -16,6 +15,42 @@ it("renderiza campos base de carta", ()=> {
         visible: true
     }
     const result = mount(<Card {...beethoven}/>)
-    expect(result.find('.back div.category').text()).toEqual("p") 
+    expect(result.find('.back div.category').text()).toEqual("p")
+    expect(result.find('.back div.description').text()).toEqual("Ludwig van Beethoven") 
+    expect(result.find('.back div.year').text()).toEqual("1770")
+})
+
+it("renderiza cardsleeve", ()=>{
+    const beethoven =
+    {
+        category: 'p',
+        description: 'Ludwig van Beethoven',
+        image: '../assets/images/cards/beethoven.jpg',
+        year: 1770,
+        visible: false
+    }
+
+    const mountCard = mount(<Card{...beethoven}/>)
+
+    const divsSleeveContainer = mountCard.find('div.sleeve-container')
+    expect(divsSleeveContainer.length).toEqual(1)
+
+    const divsSleeveView = mountCard.find('div.sleeve-view')
+    expect(divsSleeveView.length).toEqual(1)
+
+})
+
+it("card recibe 5 props", ()=>{
+    const beethoven =
+    {
+        category: 'p',
+        description: 'Ludwig van Beethoven',
+        image: '../assets/images/cards/beethoven.jpg',
+        year: 1770,
+        visible: false
+    }
+
+    const sleeveCard = mount(<Card {...beethoven}/>)
+    expect(Object.keys(sleeveCard.props()).length).toBe(5)
 
 })
