deed52f69cc40745a31835b9fb8a1cc89e049a55
Lucas Alan Silvestri <silvestrilucas@outlook.com>
2018-05-02 14:33:21 -0300

    fixed hand stories

--- src/stories/Hand.js
+++ src/stories/Hand.js
@@ -8,11 +8,11 @@ import * as cardsModule from '../model/cards/cards'
 import { Hand } from '../components/hand/Hand';
 
 const playerHandExample = [
-    { ...cardsModule.beethoven, visible: true },
-    { ...cardsModule.monaLisa, visible: true },
-    { ...cardsModule.statueOfLiberty, visible: true },
-    { ...cardsModule.discoveryOfAmerica, visible: true },
-    { ...cardsModule.telephone, visible: true },
+    { ...cardsModule.beethoven, visible: true, owner: true },
+    { ...cardsModule.monaLisa, visible: true, owner: true },
+    { ...cardsModule.statueOfLiberty, visible: true, owner: true },
+    { ...cardsModule.discoveryOfAmerica, visible: true, owner: true },
+    { ...cardsModule.telephone, visible: true, owner: true },
 ]
 
 const enemyHandExample = [
@@ -23,6 +23,10 @@ const enemyHandExample = [
     { ...cardsModule.titanic, visible: true },
 ]
 
-storiesOf('Hand', module).add('Complete Player hand', () => (<Hand cards={playerHandExample} owner={true}></Hand>))
+storiesOf('Hand', module).add('Complete Player hand', () => (<Hand
+    firstCard={playerHandExample[0]} secondCard={playerHandExample[1]} thirdCard={playerHandExample[2]}
+    fourthCard={playerHandExample[3]} fifthCard={playerHandExample[4]}></Hand>))
 
-storiesOf('Hand', module).add('Complete Enemy hand', () => (<Hand cards={enemyHandExample} owner={false}></Hand>))
+storiesOf('Hand', module).add('Complete Enemy hand', () => (<Hand
+    firstCard={enemyHandExample[0]} secondCard={enemyHandExample[1]} thirdCard={enemyHandExample[2]}
+    fourthCard={enemyHandExample[3]} fifthCard={enemyHandExample[4]}></Hand>))
