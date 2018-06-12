2cf14adc376dfabee72ebc79bd732d423c89dd4f
Lucas Alan Silvestri <silvestrilucas@outlook.com>
2018-06-12 19:19:55 -0300

    Deleted constructor in waitinggame

--- Frontend/src/components/views/waiting-game/WaitingGame.jsx
+++ Frontend/src/components/views/waiting-game/WaitingGame.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
 import './WaitingGame.css'
-import {Secciones} from './../../../model/constants/constants'
+import { Secciones } from './../../../model/constants/constants'
 
 export class WaitingGame extends Component {
 
-    constructor(props){
-        super(props)
-    }
-    render =()=> {
+    render = () => {
         return (
-        <div className="main-container-waiting-game">
-            <h2 className="text-waitin-game">Esperando partida...</h2>
-            <p className="text-waitin-game">Buscando oponente en linea... </p>
-        </div>
-        
-        
-    )}
+            <div className="main-container-waiting-game">
+                <h2 className="text-waitin-game">Esperando partida...</h2>
+                <p className="text-waitin-game">Buscando oponente en linea... </p>
+            </div>
+
+
+        )
+    }
 
     componentDidMount() {
-        setTimeout(() => {this.props.goToSection(Secciones.GAME)}, 5000)
-      }
-    
+        setTimeout(() => { this.props.goToSection(Secciones.GAME) }, 5000)
+    }
+
 }
