ea4354633c81d62a5329d939bc3349a04eee2184
Lucas Alan Silvestri <silvestrilucas@outlook.com>
2018-06-12 18:00:18 -0300

    Server implementantion feature (#15)
    * server updated
    
    * added travis yml

---
+++ Backend/src/routes/card.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { createCard } from '../model/card'
+// import mongoose from 'mongoose'
+
+// const Card = mongoose.model('card')
+
+const router = Router()
+
+const ok = { status: 'ok' }
+
+router.post('/create', async (req, res) => {
+    const item = req.body
+    const data = await createCard([item])
+    res.send({
+        ...ok,
+        data,
+    })
+})
+
+export default router
