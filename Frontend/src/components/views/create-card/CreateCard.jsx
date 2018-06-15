6a79f8570e1d0489b7530312ca789ced71feefb3
Pablo Marrero <=>
2018-06-15 14:41:25 -0300

    Added CreateCard component with basic template

---
+++ Frontend/src/components/views/create-card/CreateCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CreateCard.css'
+
+export const CreateCard = () =>
+    <div className="main-container-create-card">
+        <p>Sección donde podrás crear tus propias cartas!</p>
+        <div className="button-create-card">
+            <button type="submit" id="createCard">Crear carta</button>
+        </div>
+    </div>
+
+
