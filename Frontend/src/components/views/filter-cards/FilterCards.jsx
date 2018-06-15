85edfedabd260c9f82a86edc40456c0f697b5ac2
Pablo Marrero <=>
2018-06-15 14:41:53 -0300

    Added Filtercards component with basic template

---
+++ Frontend/src/components/views/filter-cards/FilterCards.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './FilterCards.css'
+
+export const FilterCards = () => 
+        <div className="main-container-card-list">
+            <h2>Filtro de cartas</h2>
+        </div>
