5598de09eb5ef523a6fb82274eef99422338db02
Pablo Marrero <=>
2018-06-17 20:13:57 -0300

    Tested creation card in mongoose

---
+++ Backend/src/model/card.test.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+import { Mockgoose } from 'mockgoose'
+import './card.js'
+
+const { ObjectId } = mongoose.Types
+
+const mockgoose = new Mockgoose(mongoose)
+const Card = mongoose.model('Card')
+
+describe('CardModel', () => {
+    beforeAll(async () => {
+        await mockgoose.prepareStorage()
+        await mongoose.connect('mongodb://timeline-ps:timeline-ps1@ds263670.mlab.com:63670/timeline-ps')
+    })
+    afterEach(async () => {
+        await mockgoose.helper.reset()
+    })
+
+    it('card created', async () => {
+        const dummyCard = {
+            year: 2001,
+            description: 'Television',
+            category: 'Inventions',
+            url: ''//'https://i.imgur.com/mlgIA8P.jpg'
+        }
+
+        const cardSaved = await new Card(dummyCard).save()
+
+        expect(cardSaved).toMatchObject({
+            _id: expect.any(ObjectId),
+            __v: 0,
+            year: 2001,
+            description: 'Television',
+            category: 'Inventions',
+            url: ''// 'https://i.imgur.com/mlgIA8P.jpg'
+        })
+        expect(await Card.count()).toEqual(1)
+    })
+})
