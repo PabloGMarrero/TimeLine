447e3f2e389e55aea55b64fb551ae6e51b5fd890
Pablo Marrero <=>
2018-06-20 14:21:44 -0300

    Added post method in routes

--- Backend/src/routes/card.js
+++ Backend/src/routes/card.js
@@ -4,7 +4,17 @@ import './../model/card'
 
 const router = Router()
 const Cardmodel = mongoose.model('Card')
-
+const ok = { status: 'ok' }
 router.get('/cards', async (req, res) => res.send(await Cardmodel.find({})))
+
+router.post('/cards', async (req, res) => {
+    const item = req.body
+    const data = await Cardmodel.create([item])
+    res.send({
+        ...ok,
+        data,
+    })
+})
+
 
 export default router
