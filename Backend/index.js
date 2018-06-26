fb28ffdcdc1bde28d53d18e4511795abd2312d07
Pablo Marrero <=>
2018-06-25 21:28:34 -0300

    fixed problems with mlab connection
    really dont know what happened

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 import start from './src/server/server'
 
-const port = process.env.PORT || 30001
+const port = process.env.PORT || 3001
 
 start().listen(port, () => {
     console.log(`Timeline backend listening on port ${port}!`)
